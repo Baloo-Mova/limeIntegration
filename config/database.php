dc7b24d850a7e686e295e71926999970590085a3
solomon <sergious-91@mail.ru>
2017-08-17 18:49:09 +0300

    qwe

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
         'mysql_lime' => [
@@ -64,7 +64,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
 
