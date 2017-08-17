bdf2f35c1ef90f45e770282fa6dc85382b0ae2d5
solomon <sergious-91@mail.ru>
2017-08-17 22:08:54 +0300

    qwe

--- app/Http/Controllers/Admin/AdminSurveysController.php
+++ app/Http/Controllers/Admin/AdminSurveysController.php
@@ -32,7 +32,7 @@ class AdminSurveysController extends Controller
         $surveys = LimeSurveys::all();
 
         return view('admin.surveys.statistics', [
-            'finished' => isset($finished) ? $finished : 0 ,
+            'finished' => isset($finished) ? $finished : 0,
             'not_finished' => isset($not_finished) ? $not_finished : 0,
             'surveys' => isset($surveys) ? $surveys : []
         ]);
@@ -42,16 +42,16 @@ class AdminSurveysController extends Controller
     {
         $survey = LimeSurveys::where(['sid' => $sid])->first();
 
-        if(!isset($survey)){
+        if (!isset($survey)) {
             return back();
         }
 
         $lime_base = DB::connection('mysql_lime');
         $lime_base->table('surveys')->where('sid', $sid)->update(['type_id' => $type]);
 
-        if($type == 0){
+        if ($type == 0) {
             $res = $this->addParticipantsToWorksheet($sid);
-        }else{
+        } else {
             $res = $this->deleteParticipantsFromWorksheet($sid);
         }
 
@@ -64,12 +64,12 @@ class AdminSurveysController extends Controller
         $schemaConnAdmin = Schema::connection('mysql_lime');
 
         $partisipants = LimeParticipants::all();
-        if(!isset($partisipants)){
+        if (!isset($partisipants)) {
             return false;
         }
 
-        if(!$schemaConnAdmin->hasTable('tokens_'.$sid)){
-            $schemaConnAdmin->create('tokens_'.$sid, function (Blueprint $table) {
+        if (!$schemaConnAdmin->hasTable('tokens_' . $sid)) {
+            $schemaConnAdmin->create('tokens_' . $sid, function (Blueprint $table) {
                 $table->increments('tid');
                 $table->string('participant_id', 50)->nullable();
                 $table->string('firstname', 150)->nullable();
@@ -92,44 +92,44 @@ class AdminSurveysController extends Controller
 
         $res = [];
 
-        foreach ($partisipants as $p){
-            $participant = $lime_base->table('tokens_'.$sid)->where(['participant_id' => $p->participant_id])->first();
+        foreach ($partisipants as $p) {
+            $participant = $lime_base->table('tokens_' . $sid)->where(['participant_id' => $p->participant_id])->first();
 
-            if(isset($participant)){
+            if (isset($participant)) {
                 continue;
             }
 
-            $t = $lime_base->table('tokens_'.$sid)->insertGetId([
+            $t = $lime_base->table('tokens_' . $sid)->insertGetId([
                 'participant_id' => $p->participant_id,
                 'firstname' => $p->firstname,
                 'lastname' => $p->lastname,
-                'email'    => $p->email,
-                'token'    => $this->gen_uuid(),
+                'email' => $p->email,
+                'token' => $this->gen_uuid(),
                 'emailstatus' => 'OK'
             ]);
             $res[] = [
                 'participant_id' => $p->participant_id,
-                'token_id'       => $t,
-                'survey_id'      => $sid,
-                'date_created'   => Carbon::now()->toDateTimeString()
+                'token_id' => $t,
+                'survey_id' => $sid,
+                'date_created' => Carbon::now()->toDateTimeString()
             ];
 
             $user_for_notificate = User::where('ls_participant_id', $p->participant_id)->first();
-            if(!isset($user_for_notificate)){
+            if (!isset($user_for_notificate)) {
                 continue;
             }
-            $token = $lime_base->table('tokens_'.$sid)->where('tid', $t)->first();
-            if(!isset($token)){
+            $token = $lime_base->table('tokens_' . $sid)->where('tid', $t)->first();
+            if (!isset($token)) {
                 continue;
             }
-            $url = "/gotosurvey/".$sid."/".$token->token;
+            $url = "/gotosurvey/" . $sid . "/" . $token->token;
             $message = [
                 'text' => "Для Вас доступен новый опрос.",
-                'greeting' => "Здравствуйте, ".$user_for_notificate->name." ".$user_for_notificate->second_name."!",
+                'greeting' => "Здравствуйте, " . $user_for_notificate->name . " " . $user_for_notificate->second_name . "!",
                 'action_title' => "Пройти опрос",
                 'subject' => "Вам доступен новый опрос",
                 'url' => $url,
-                'button' => "Вы можете пройти его по <a href='".url($url)."'>этой ссылке</a>",
+                'button' => "Вы можете пройти его по <a href='" . url($url) . "'>этой ссылке</a>",
             ];
             $user_for_notificate->notify(new UserNotification($message, 'mail'));
 
@@ -147,25 +147,25 @@ class AdminSurveysController extends Controller
         $schemaConnAdmin = Schema::connection('mysql_lime');
 
         $partisipants = LimeParticipants::all();
-        if(!isset($partisipants)){
+        if (!isset($partisipants)) {
             return false;
         }
 
-        if(!$schemaConnAdmin->hasTable('tokens_'.$sid)){
+        if (!$schemaConnAdmin->hasTable('tokens_' . $sid)) {
             return true;
         }
 
-        foreach ($partisipants as $p){
-            $participant = $lime_base->table('tokens_'.$sid)->where(['participant_id' => $p->participant_id])->first();
-            if(!isset($participant)){
+        foreach ($partisipants as $p) {
+            $participant = $lime_base->table('tokens_' . $sid)->where(['participant_id' => $p->participant_id])->first();
+            if (!isset($participant)) {
                 continue;
             }
             $lime_base->table('survey_links')->where([
                 'participant_id' => $p->participant_id,
-                'token_id'       => $participant->tid,
-                'survey_id'      => $sid
+                'token_id' => $participant->tid,
+                'survey_id' => $sid
             ])->delete();
-            $lime_base->table('tokens_'.$sid)->where(['participant_id' => $p->participant_id])->delete();
+            $lime_base->table('tokens_' . $sid)->where(['participant_id' => $p->participant_id])->delete();
         }
 
         return true;
@@ -188,14 +188,14 @@ class AdminSurveysController extends Controller
         $sid = $request->get('sid');
         $money = $request->get('money');
 
-        if(!isset($sid) || !isset($money)){
+        if (!isset($sid) || !isset($money)) {
             Toastr::error('Не указан обязательный параметр!', 'Ошибка');
             return back();
         }
 
         $survey = LimeSurveys::where(['sid' => $sid])->first();
 
-        if(!isset($survey)){
+        if (!isset($survey)) {
             Toastr::error('Опрос не найден!', 'Ошибка');
             return back();
         }
@@ -210,26 +210,30 @@ class AdminSurveysController extends Controller
     public function remind($sid)
     {
         $lime_base = DB::connection('mysql_lime');
-        $users = $lime_base->table('tokens_'.$sid)->where('completed', 'N')->get();
-
-        if(!isset($users)){
+        try {
+            $users = $lime_base->table('tokens_' . $sid)->where('completed', 'N')->get();
+        } catch (\Exception $ex) {
+            Toastr::error('Не найдено непрошедших этот опрос пользователей!', 'Ошибка!');
+            return back();
+        }
+        if (!isset($users)) {
             Toastr::error('Не найдено непрошедших этот опрос пользователей!', 'Ошибка!');
             return back();
         }
 
-        foreach ($users as $user){
+        foreach ($users as $user) {
             $u = User::where('ls_participant_id', $user->participant_id)->first();
-            if(!isset($u)){
+            if (!isset($u)) {
                 continue;
             }
-            $url = "/gotosurvey/".$sid."/".$user->token;
+            $url = "/gotosurvey/" . $sid . "/" . $user->token;
             $message = [
                 'text' => "Напоминаем, что для Вас доступен новый опрос.",
-                'greeting' => "Здравствуйте, ".$u->name." ".$u->second_name."!",
+                'greeting' => "Здравствуйте, " . $u->name . " " . $u->second_name . "!",
                 'action_title' => "Пройти опрос",
                 'subject' => "Напоминание об опросе",
                 'url' => $url,
-                'button' => "Вы можете пройти его по <a href='".url($url)."'>этой ссылке</a>",
+                'button' => "Вы можете пройти его по <a href='" . url($url) . "'>этой ссылке</a>",
             ];
             $u->notify(new UserNotification($message, 'mail'));
         }
